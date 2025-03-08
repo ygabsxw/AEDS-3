e1be1ba17bbfbe4bfe6baa0074e4bdb0a354861e
sevak19 <tu1227csn@gmail.com>
2025-03-08 03:25:03 -0300

    feat: main

---
+++ src/Main.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+
+public class Main {
+
+public static void main(String[] args) {
+
+    Scanner console;
+
+    try {
+        console = new Scanner(System.in);
+        int opcao;
+        do {
+
+            System.out.println("\n\nAEDsIII");
+            System.out.println("-------");
+            System.out.println("> Início");
+            System.out.println("\n1 - Filmes");
+            System.out.println("0 - Sair");
+
+            System.out.print("\nOpção: ");
+            try {
+                opcao = Integer.valueOf(console.nextLine());
+            } catch(NumberFormatException e) {
+                opcao = -1;
+            }
+
+            switch (opcao) {
+                case 1:
+                    try{
+                        (new MovieMenu()).menu();
+                    } catch(Exception e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+                    break;
+            }
+
+        } while (opcao != 0);
+
+
+
+    } catch(Exception e) {
+        e.printStackTrace();
+    }
+
+}
+
+}
